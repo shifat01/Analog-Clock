183ce720fb9d2b75377d32055eef1f2efeef893a
shifat01 <shifat333000@gmail.com>
2025-10-20 17:06:26 +0600

    watch is working now

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const body = document.querySelector("body"),
   secondHand = document.querySelector(".second"),
   modeSwitch = document.querySelector(".mode-switch");
 
+
+//dark mode
 if (localStorage.getItem("mode") === "Dark Mode") {
   body.classList.add("dark");
   modeSwitch.textContent = "Light Mode";
@@ -18,3 +20,21 @@ modeSwitch.addEventListener("click", () => {
   modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
   localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
 });
+
+
+//time
+const updateTime = () => {
+  let date = new Date(),
+  secToDeg = (date.getSeconds() /60) * 360,
+  minToDeg = (date.getMinutes() /60) * 360,
+  hrToDeg = (date.getHours() /12) * 360;
+
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  houHand.style.transform = `rotate(${hrToDeg}deg)`;
+
+}
+
+setInterval(updateTime, 1000);
+
+updateTime()
